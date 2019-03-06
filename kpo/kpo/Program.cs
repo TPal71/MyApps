053e0e288e6b38f807bc5b56cb1a053217cd5424
TPal71 <treso.pal@trexsys.hu>
2019-03-06 12:50:04 +0100

    #2: Kiértékelés és pontok, állás kiirása

--- kpo/kpo/Program.cs
+++ kpo/kpo/Program.cs
@@ -68,6 +68,32 @@ namespace kpo
                         compChoice = "olló";
                         break;
                 }
+                ///Feladat 5: Értékeljük ki a compChoice és a playerChoice 
+                ///változókat
+                ///
+                if (
+                   (playerChoice == "kő" && compChoice == "papir") ||
+                   (playerChoice == "papir" && compChoice == "olló") ||
+                   (playerChoice == "olló" && compChoice == "kő")
+                   )
+                {
+                    Console.WriteLine("Számítógép: " + compChoice + " Te: " + playerChoice);
+                    Console.WriteLine("Veszitettél! Állás: Szg: {0} Játékos: {1}", ++compScore, playerScore);
+
+                }
+                else if (playerChoice == compChoice)
+                {
+                    Console.WriteLine("Számítógép: " + compChoice + " Te: " + playerChoice);
+                    Console.WriteLine("Döntetlen! Állás: Szg: {0} Játékos: {1}", compScore, playerScore);
+                }
+                else
+                {
+                    Console.WriteLine("Számítógép: " + compChoice + " Te: " + playerChoice);
+                    Console.WriteLine("Nyertél! Állás: Szg: {0} Játékos: {1}", compScore, ++playerScore);
+
+                }
+
+
 
 
                 Console.Write("Akarsz még játszani? i/n");
