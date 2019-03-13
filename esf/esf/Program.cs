ba3c1a68f03a23a9f02831e3dc37e7556c6c3928
TPal71 <treso.pal@trexsys.hu>
2019-03-13 12:05:12 +0100

    #4: Ciklusok

--- esf/esf/Program.cs
+++ esf/esf/Program.cs
@@ -10,6 +10,8 @@ namespace esf
     {
         static void Main(string[] args)
         {
+            int v = 0;
+
             //A létregozott új tipussal inicializálunk egy objektumot
             Excercise ex = new Excercise("Tresó Pál");
 
@@ -23,6 +25,31 @@ namespace esf
             
             //Ezt kellene megoldani
             Console.WriteLine(ex.getName());
+
+            ///Ciklusok
+            //For ciklus
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Ciklus:<Változó hivatkozás I.>" + i);
+                Console.WriteLine("Ciklus:<Változó hivatkozás II.> {0}", i);
+            }
+
+            //While ciklus, előltesztelős ciklus
+            v = 2;
+            while (v>0)
+            {
+                Console.WriteLine("While ciklus");
+                v--;
+            }
+
+            //Do-While: Hátultesztelős ciklus
+            v = 2;
+            do
+            {
+                Console.WriteLine("Do While ciklus");
+                v--;
+            } while (v>0);
+            
 
             
             //Beolvas egy karakert a konzolról
