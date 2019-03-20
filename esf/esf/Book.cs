13b6143ff888f5338b73610b35570031c58193a7
TPal71 <treso.pal@trexsys.hu>
2019-03-20 12:16:55 +0100

    #4: Tipusos listák

---
+++ esf/esf/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace esf
+{
+    internal class Book
+    {
+        public string Name { get; internal set; }
+        public string Author { get; internal set; }
+        public int Page { get; internal set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Book objAsPart = obj as Book;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+
+        public bool Equals(Book other)
+        {
+            if (other == null) return false;
+            return (this.Page.Equals(other.Page));
+        }
+    }
+}
--- esf/esf/Program.cs
+++ esf/esf/Program.cs
@@ -13,6 +13,10 @@ namespace esf
             int v = 0;
             string s;
 
+
+            
+            
+
             //A létregozott új tipussal inicializálunk egy objektumot
             Excercise ex = new Excercise("Tresó Pál");
 
@@ -86,6 +90,75 @@ namespace esf
             s = Console.ReadLine();
             Console.WriteLine("Beolvasás konzolról:" + s);
 
+            //Egy billentyű leütése
+            // Console.ReadKey(true).KeyChar
+            // integerré konvertálni a console számot(string)
+            //int.Parse(Console.ReadLine())
+
+
+            ///Tömbök
+            ///Létrehoztunk egy int elemeket tartalmazó tömböt, aminek a neve
+            ///'szamok'
+            int[] szamok = new int[5];
+            string[] szovegek = new string[10];
+
+            //Tömb inicializálás
+            int[] iNums = new int[] { 1,2,3,4,5,6};
+            string[] sFruit = new string[] {"alma","körte","banán","narancs" };
+            Console.WriteLine("iNums első eleme: {0} sFruit első eleme:{1}", iNums[0],sFruit[0]);
+            Console.WriteLine("iNums első eleme:" + iNums[0] + " sFruit első eleme:" + sFruit[0]);
+
+
+
+
+            ///Tipusos listák: List<T>
+            //Lista elem 
+            var Lista = new List<Book>();
+
+
+            //Új elemek hozzáadása a listához
+            //Ctrl-'.' hivhatjuk segítségül
+            Lista.Add(new Book() { Name = "Oroszlánkölykök", Author = "Irwin Shaw",Page=100 });
+            Lista.Add(new Book() { Name = "Megkergetve", Author = "Margó István" ,Page=200});
+            Lista.Add(new Book() { Name = "Egri csillagok", Author = "Gárdonyi Géza", Page = 1200 });
+
+            
+            //Lista kiiratása
+            foreach (Book book in Lista)
+            {
+                Console.WriteLine(book.Name + " " +book.Author + " " + book.Page );
+            }
+
+            //Lista egy elemének törlése
+            Lista.Remove(new Book() { Name = "Megkergetve", Author = "Margó István", Page = 200 });
+
+
+            Console.WriteLine("***************************************************************************");
+            //Lista kiiratása
+            foreach (Book book in Lista)
+            {
+                Console.WriteLine(book.Name + " " + book.Author + " " + book.Page);
+            }
+            Lista.Remove(Lista.Find(x => x.Name.Contains("Oroszlánkölykök")));
+            //Lista.Find(x => x.Name.Contains("Oroszlánkölykök"))
+            //Console.WriteLine("Keressük az Oroszlánkölykök könyvet:" + Lista.Find(x => x.Name.Contains("Oroszlánkölykök")).Name);
+
+            Console.WriteLine("***************************************************************************");
+
+            //Lista kiiratása
+            foreach (Book book in Lista)
+            {
+                Console.WriteLine(book.Name + " " + book.Author + " " + book.Page);
+            }
+            /*Oldalszám keresés
+            Console.WriteLine("Tartalmaz(): {0}",
+            Lista.Contains(new Book { Name="",Author="", Page=100}));
+            */
+
+            //Keresés cim alapján
+
+            // Console.WriteLine("\nFind: Könyv cime tartalmaz \"Oroszlánkölykök\": {0}",
+            //     Lista.Find(x => x.Name.Contains("Oroszlánkölykök")).Name);
 
             //Beolvas egy karakert a konzolról
             Console.ReadKey();
